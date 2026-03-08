e699e62bee8366b650c9c655ee704b16949e2b68
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-03-08 17:08:19 +0100

    feat: make generate page logo navigate back to hero

--- src/app/pages/generate-recipe-page/generate-recipe-page.ts
+++ src/app/pages/generate-recipe-page/generate-recipe-page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-generate-recipe-page',
+  imports: [RouterLink],
   templateUrl: './generate-recipe-page.html',
   styleUrl: './generate-recipe-page.scss',
 })
