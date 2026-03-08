fafcb2ba7583114203ba4d7037a24a8c25345bfa
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-03-08 17:07:05 +0100

    feat: add generate recipe page route and intro header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GenerateRecipePageComponent } from './pages/generate-recipe-page/generate-recipe-page';
+import { HomePageComponent } from './pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'generate-recipe', component: GenerateRecipePageComponent },
+];
---
+++ src/app/pages/generate-recipe-page/generate-recipe-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-recipe-page',
+  templateUrl: './generate-recipe-page.html',
+  styleUrl: './generate-recipe-page.scss',
+})
+export class GenerateRecipePageComponent {}
