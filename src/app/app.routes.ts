4883db4835ab3705ece82b40e7c688de1e9601f8
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-03-23 08:58:08 +0100

    feat: add the loading page and link it from the generate button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { GenerateRecipePageComponent } from './pages/generate-recipe-page/generate-recipe-page';
 import { HomePageComponent } from './pages/home-page/home-page';
+import { LoadingPageComponent } from './pages/loading-page/loading-page';
 import { PreferencesPageComponent } from './pages/preferences-page/preferences-page';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'generate-recipe', component: GenerateRecipePageComponent },
   { path: 'preferences', component: PreferencesPageComponent },
+  { path: 'loading', component: LoadingPageComponent },
 ];
