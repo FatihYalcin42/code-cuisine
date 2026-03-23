d861c890db519cb60e07d0b7fc31861299cb829d
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-03-23 07:55:49 +0100

    Add the preferences page with navigation and portion/person selectors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { GenerateRecipePageComponent } from './pages/generate-recipe-page/generate-recipe-page';
 import { HomePageComponent } from './pages/home-page/home-page';
+import { PreferencesPageComponent } from './pages/preferences-page/preferences-page';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'generate-recipe', component: GenerateRecipePageComponent },
+  { path: 'preferences', component: PreferencesPageComponent },
 ];
---
+++ src/app/pages/preferences-page/preferences-page.ts
@@ -0,0 +1,29 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-preferences-page',
+  imports: [RouterLink],
+  templateUrl: './preferences-page.html',
+  styleUrl: './preferences-page.scss',
+})
+export class PreferencesPageComponent {
+  protected readonly portions = signal(2);
+  protected readonly persons = signal(2);
+
+  protected decreasePortions(): void {
+    this.portions.update((value) => Math.max(1, value - 1));
+  }
+
+  protected increasePortions(): void {
+    this.portions.update((value) => value + 1);
+  }
+
+  protected decreasePersons(): void {
+    this.persons.update((value) => Math.max(1, value - 1));
+  }
+
+  protected increasePersons(): void {
+    this.persons.update((value) => value + 1);
+  }
+}
