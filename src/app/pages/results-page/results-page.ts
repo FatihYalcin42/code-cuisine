0687587eb52c6b53b9e063eda8cd662ab160a2da
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-03-24 10:15:16 +0100

    Restore hero navigation on results page logo

--- src/app/pages/results-page/results-page.ts
+++ src/app/pages/results-page/results-page.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { RecipeGenerationService } from '../../services/recipe-generation.service';
 
 @Component({
   selector: 'app-results-page',
+  imports: [RouterLink],
   templateUrl: './results-page.html',
   styleUrl: './results-page.scss',
 })
