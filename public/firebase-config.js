4fbaf631e034fefdb66682bb6a1bbb766b6af3f0
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-03-27 12:23:18 +0100

    Wire app to Firebase config and add Firestore-backed cookbook store

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,8 +1,8 @@
 window.__CODE_CUISINE_FIREBASE__ = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyA-LPRZnRxBHIsiI8XUkqQ8M3ew4T-TTeU',
+  authDomain: 'code-a-cuisine-beec5.firebaseapp.com',
+  projectId: 'code-a-cuisine-beec5',
+  storageBucket: 'code-a-cuisine-beec5.firebasestorage.app',
+  messagingSenderId: '141813323661',
+  appId: '1:141813323661:web:a9a5e9f5b63bfb9d754fc4',
 };
