f43f158cc85282a23bae28b234cfae08d5c1ab76
Fatih Yalcin <86c29vxm24@privaterelay.appleid.com>
2026-03-28 08:29:01 +0100

    Stop tracking local Firebase config and ignore admin/service credential files

--- public/firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.__CODE_CUISINE_FIREBASE__ = {
-  apiKey: 'AIzaSyA-LPRZnRxBHIsiI8XUkqQ8M3ew4T-TTeU',
-  authDomain: 'code-a-cuisine-beec5.firebaseapp.com',
-  projectId: 'code-a-cuisine-beec5',
-  storageBucket: 'code-a-cuisine-beec5.firebasestorage.app',
-  messagingSenderId: '141813323661',
-  appId: '1:141813323661:web:a9a5e9f5b63bfb9d754fc4',
-};
